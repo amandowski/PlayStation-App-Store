83919ed746578e7197480e869f7cb1bc265a8f53
Zach Wormstadt <zahary.wormstadt@jacks.sdstate.edu>
2021-03-01 12:21:43 -0600

    added header buttons and dropdown menu

--- App/playstation_app_store/lib/main.dart
+++ App/playstation_app_store/lib/main.dart
@@ -16,9 +16,62 @@ class MyFirstApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: AppBar(
-            title: Text('My First App'),
+            title: Text("PlayStation"),
+            leading: GestureDetector(
+              child: PopupMenuButton(
+                offset: Offset(0, 55),
+                icon: Icon(Icons.menu),
+                itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+                  PopupMenuItem(
+                    child: ListTile(
+                      title: Text('Consoles'),
+                      onTap: () {/* Write listener code here */},
+                    ),
+                  ),
+                  PopupMenuItem(
+                    child: ListTile(
+                      title: Text('Accessories'),
+                      onTap: () {/* Write listener code here */},
+                    ),
+                  ),
+                  PopupMenuItem(
+                    child: ListTile(
+                      title: Text('My Account'),
+                      onTap: () {/* Write listener code here */},
+                    ),
+                  ),
+                  PopupMenuItem(
+                    child: ListTile(
+                      title: Text('Help'),
+                      onTap: () {/* Write listener code here */},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              Padding(
+                  padding: EdgeInsets.only(right: 20.0),
+                  child: GestureDetector(
+                    onTap: () {/* Write listener code here */},
+                    child: Icon(
+                      Icons.shopping_cart,
+                      size: 26.0,
+                    ),
+                  )),
+              Padding(
+                  padding: EdgeInsets.only(right: 20.0),
+                  child: GestureDetector(
+                    onTap: () {/* Write listener code here */},
+                    child: Icon(
+                      Icons.account_circle,
+                      size: 26.0,
+                    ),
+                  )),
+            ],
           ),
           body: Column(
             children: [

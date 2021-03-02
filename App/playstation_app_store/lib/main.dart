5b88fee6da9fac351be306ea794f06a710f489d6
Amanda Lewandowski <49411229+amandowski@users.noreply.github.com>
2021-03-02 16:30:46 -0600

    override

--- App/playstation_app_store/lib/main.dart
+++ App/playstation_app_store/lib/main.dart
@@ -18,12 +18,68 @@ class MyFirstApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
+<<<<<<< HEAD
             title: Text('PlayStation',
                 style: TextStyle(
                   fontSize: 30.0,
                 )),
             centerTitle: true,
             backgroundColor: Colors.indigo[900],
+=======
+            title: Text("PlayStation"),
+            leading: GestureDetector(
+              child: PopupMenuButton(
+                offset: Offset(0, 55),
+                icon: Icon(Icons.menu),
+                itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+                  PopupMenuItem(
+                    child: ListTile(
+                      title: Text('Consoles'),
+                      onTap: () {/* Write listener code here */},
+                    ),
+                  ),
+                  PopupMenuItem(
+                    child: ListTile(
+                      title: Text('Accessories'),
+                      onTap: () {/* Write listener code here */},
+                    ),
+                  ),
+                  PopupMenuItem(
+                    child: ListTile(
+                      title: Text('My Account'),
+                      onTap: () {/* Write listener code here */},
+                    ),
+                  ),
+                  PopupMenuItem(
+                    child: ListTile(
+                      title: Text('Help'),
+                      onTap: () {/* Write listener code here */},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              Padding(
+                  padding: EdgeInsets.only(right: 20.0),
+                  child: GestureDetector(
+                    onTap: () {/* Write listener code here */},
+                    child: Icon(
+                      Icons.shopping_cart,
+                      size: 26.0,
+                    ),
+                  )),
+              Padding(
+                  padding: EdgeInsets.only(right: 20.0),
+                  child: GestureDetector(
+                    onTap: () {/* Write listener code here */},
+                    child: Icon(
+                      Icons.account_circle,
+                      size: 26.0,
+                    ),
+                  )),
+            ],
+>>>>>>> parent of 78cd6b9 (added console screen)
           ),
           body: Column(
             children: [

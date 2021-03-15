1d00ba86b334363edf13cba2a6beccda1589f43f
Zach Wormstadt <zahary.wormstadt@jacks.sdstate.edu>
2021-03-15 12:12:29 -0500

    added routing to checkout from cart

--- App/playstation_app_store/lib/cart.dart
+++ App/playstation_app_store/lib/cart.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:playstation_app_store/accessories.dart';
+import 'package:playstation_app_store/checkout.dart';
 import 'package:playstation_app_store/consoles.dart';
 import 'package:playstation_app_store/account.dart';
+import 'package:playstation_app_store/main.dart';
 
 class Cart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     int counter = 1;
-        return MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
@@ -97,197 +99,201 @@ class Cart extends StatelessWidget {
           ],
         ),
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            children: <Widget>[
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(children: <Widget>[
               Positioned(
                   top: 75.0,
                   //left: 15.0,
                   child: Center(
-                  child: Text(
+                      child: Text(
                     'CART',
                     textAlign: TextAlign.center,
                     style: TextStyle(
                         fontFamily: 'Montserrat',
                         fontSize: 35.0,
                         fontWeight: FontWeight.bold),
-                  )
-                  )
-                  ),
-                  SizedBox (height: 12.0),
-                  Row(
-                    children: <Widget>[
-                      Container(
-                       width: 80.0,
-                       height: 80.0,
-                       decoration: BoxDecoration(
-                       color: Colors.red,
-                       image: DecorationImage(
-                       image: ExactAssetImage(" "),
-                       fit: BoxFit.fitHeight,
-                       ),
-                      ),
-                     ),
-                     SizedBox (height: 12.0),
-                     Column(children: <Widget>[
-                      Container(
-                           width: 100.0,
-                           child: Text (
-                              "xyz",
-                              textAlign: TextAlign.right,
-                             style: TextStyle(
-                             fontWeight: FontWeight.bold,
-                             fontSize: 20.0,
-                              ),
-                           ),
-                         ),
-                  ]
-                  ),
-                  ]
-                  ),
-                  SizedBox (height: 12.0),
-                  Row(
-                    children: <Widget>[
-                      Container(
-                    width: 25.0,
-                    height: 25.0,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      borderRadius: BorderRadius.circular(4.0),
+                  ))),
+              SizedBox(height: 12.0),
+              Row(children: <Widget>[
+                Container(
+                  width: 80.0,
+                  height: 80.0,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    image: DecorationImage(
+                      image: ExactAssetImage(" "),
+                      fit: BoxFit.fitHeight,
                     ),
-                    child: GestureDetector(
-                      onTap: (){
-                        counter+1;
-                      },
-                      child: Icon(
+                  ),
+                ),
+                SizedBox(height: 12.0),
+                Column(children: <Widget>[
+                  Container(
+                    width: 100.0,
+                    child: Text(
+                      "xyz",
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ]),
+              ]),
+              SizedBox(height: 12.0),
+              Row(children: <Widget>[
+                Container(
+                  width: 25.0,
+                  height: 25.0,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  child: GestureDetector(
+                    onTap: () {
+                      counter + 1;
+                    },
+                    child: Icon(
                       Icons.add,
                       color: Colors.white,
-                      size:20.0,
-                    ),
+                      size: 20.0,
                     ),
                   ),
-                  //print (counter),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Text(
+                ),
+                //print (counter),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text(
                     "1",
                     style: TextStyle(
                       fontSize: 20.0,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
+                ),
+                Container(
+                  width: 25.0,
+                  height: 25.0,
+                  decoration: BoxDecoration(
+                    color: Colors.blue[300],
+                    borderRadius: BorderRadius.circular(4.0),
                   ),
-                  Container(
-                    width: 25.0,
-                    height: 25.0,
-                    decoration: BoxDecoration(
-                      color: Colors.blue[300],
-                      borderRadius: BorderRadius.circular(4.0),
-                    ),
-                    child: GestureDetector(
-                      onTap: (){
-                        counter-1;
-                      },
-                      child: Icon(
+                  child: GestureDetector(
+                    onTap: () {
+                      counter - 1;
+                    },
+                    child: Icon(
                       Icons.remove,
                       color: Colors.white,
-                      size:20.0,
-                      ),
+                      size: 20.0,
                     ),
-                     ),
-                     Spacer(),
-                     RaisedButton(
+                  ),
+                ),
+                Spacer(),
+                RaisedButton(
+                  child: Text(
+                    'Delete',
+                    style: TextStyle(color: Colors.red[900], fontSize: 20),
+                  ),
+                  onPressed: () {
+                    //Navigator.pop(context);
+                  },
+                ),
+                Spacer(),
+                RaisedButton(
+                  child: Text(
+                    'Save for later',
+                    style: TextStyle(color: Colors.red[900], fontSize: 20),
+                  ),
+                  onPressed: () {
+                    // Navigator.pop(context);
+                  },
+                ),
+              ]),
+              SizedBox(height: 12.0),
+              Row(
+                children: <Widget>[
+                  Text(
+                    "Price: ",
+                    style: TextStyle(
+                        color: Colors.red[900],
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              Divider(
+                color: Colors.black,
+              ),
+              SizedBox(height: 12.0),
+              Column(children: <Widget>[
+                Container(
+                  width: 100.0,
+                  child: Center(
                       child: Text(
-                        'Delete',
-                        style: TextStyle(color: Colors.red[900], fontSize: 20),
-                      ),
-                      onPressed: () {
-                         //Navigator.pop(context);
-                        },
+                    'ORDER SUMMARY',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
                     ),
-                    Spacer(),
-                    RaisedButton(
-                      child: Text(
-                        'Save for later',
-                        style: TextStyle(color: Colors.red[900], fontSize: 20),
-                      ),
-                      onPressed: () {
-                        // Navigator.pop(context);
-                        },
-                    ),
-                    ]
+                  )),
+                ),
+              ]),
+              SizedBox(height: 12.0),
+              Row(
+                children: <Widget>[
+                  Text(
+                    "Subtotal: ",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
                   ),
-                  SizedBox (height: 12.0),
-                  Row(
-                    children: <Widget>[
-                      Text(
-                        "Price: ",
-                        style: TextStyle(color: Colors.red[900], fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                    ],
+                ],
+              ),
+              SizedBox(height: 12.0),
+              Row(
+                children: <Widget>[
+                  Text(
+                    "Discount: ",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
                   ),
-                  Divider( color: Colors.black,),
-                  SizedBox (height: 12.0),
-                  Column(children: <Widget>[
-                   Container(
-                           width: 100.0,
-                           child: Center(
-                             child: Text(
-                              'ORDER SUMMARY',
-                              textAlign: TextAlign.center,
-                             style: TextStyle(
-                             fontWeight: FontWeight.bold,
-                             fontSize: 20.0,
-                              ),
-                             ) 
-                           ), 
-                         ),
-                  ]
+                ],
+              ),
+              SizedBox(height: 12.0),
+              Row(children: <Widget>[
+                RaisedButton(
+                  child: Text(
+                    'Continue Shopping',
+                    style: TextStyle(color: Colors.red[900], fontSize: 20),
                   ),
-                  SizedBox (height: 12.0),
-                  Row(
-                    children: <Widget>[
-                      Text(
-                           "Subtotal: " ,
-                           style: TextStyle(color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold),
-                         ),
-                    ],
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => PlayStationApp()),
+                    );
+                  },
+                ),
+                Spacer(),
+                RaisedButton(
+                  child: Text(
+                    'Checkout',
+                    style: TextStyle(color: Colors.red[900], fontSize: 20),
                   ),
-                  SizedBox (height: 12.0),
-                  Row(
-                    children: <Widget>[
-                      Text(
-                           "Discount: ",
-                           style: TextStyle(color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold),
-                         ),
-                    ],
-                  ),
-                  SizedBox (height: 12.0),
-                  Row(children: <Widget>[
-                    RaisedButton(
-                      child: Text(
-                        'Continue Shopping',
-                        style: TextStyle(color: Colors.red[900], fontSize: 20),
-                      ),
-                      onPressed: () {
-                         //Navigator.pop(context);
-                        },
-                    ),
-                    Spacer(),
-                    RaisedButton(
-                      child: Text(
-                        'Checkout',
-                        style: TextStyle(color: Colors.red[900], fontSize: 20),
-                      ),
-                      onPressed: () {
-                        // Navigator.pop(context);
-                        },
-                    ),
-                  ]
-                  ),
-            ]
-          )
-        ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Checkout()),
+                    );
+                  },
+                ),
+              ]),
+            ])),
       ),
     );
   }

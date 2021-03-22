6adcc1dbb9df08cafad1b9dab7d52a4a5913b857
Akash9179 <56610745+Akash9179@users.noreply.github.com>
2021-03-21 21:01:21 -0500

    modifying total

--- App/playstation_app_store/lib/total.dart
+++ App/playstation_app_store/lib/total.dart
@@ -9,11 +9,124 @@ import 'package:playstation_app_store/main.dart';
 class Total extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Total Summary"),
-        backgroundColor: Colors.indigo[900],
+    getTotalAmount();
+    return Column(children: <Widget>[
+      ListView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        itemCount: cartItem.length,
+        itemBuilder: (context, index) {
+          cartItem[index].subTotal = double.parse(cartItem[index].cartPrice) *
+              cartItem[index].quantity;
+          return Column(children: <Widget>[
+            Row(children: <Widget>[
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  image: DecorationImage(
+                    image: ExactAssetImage(cartItem[index].cartImage),
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+              ),
+              SizedBox(height: 12.0),
+              Column(children: <Widget>[
+                Container(
+                  width: 150.0,
+                  child: Text(
+                    cartItem[index].cartItemName,
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ]),
+            ]),
+            Row(children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  "Quantity: " + cartItem[index].quantity.toString(),
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ]),
+            Row(
+              children: <Widget>[
+                Text(
+                  "Price: \$ " + cartItem[index].subTotal.toString(),
+                  style: TextStyle(
+                      color: Colors.red[900],
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ]);
+        },
       ),
-    );
+      SizedBox(height: 12.0),
+      SizedBox(height: 12.0),
+      Divider(
+        color: Colors.black,
+      ),
+      SizedBox(height: 12.0),
+      Column(children: <Widget>[
+        Container(
+          width: 100.0,
+          child: Center(
+              child: Text(
+            'TOTAL SUMMARY',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0,
+            ),
+          )),
+        ),
+      ]),
+      SizedBox(height: 12.0),
+      Row(
+        children: <Widget>[
+          Text(
+            "Subtotal: \$" + totalAmount.toString(),
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+      SizedBox(height: 12.0),
+      Row(
+        children: <Widget>[
+          Text(
+            "Discount: \$" + disCountedAmount.toStringAsFixed(2),
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+      Row(
+        children: <Widget>[
+          Text(
+            "Total: \$" + (totalAmount - disCountedAmount).toStringAsFixed(2),
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    ]);
   }
 }
